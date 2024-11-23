a6087a671ef9d2a7325a98be0d7ae67773473bb4
drestrepo <94070656+drestrepob@users.noreply.github.com>
2024-11-23 17:33:19 -0500

    Revert "Feature: Infrastructure layer (first settings) (#1)"
    This reverts commit 9bab98b4de50d923774d58c989dbad99433c9e97.

--- bartender/domain/models/mixins/as_dict.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import asdict, dataclass
-
-
-@dataclass
-class DictSerializerMixin:
-    def as_dict(self):
-        return asdict(self)  # noqa
--- bartender/domain/models/tag.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-from bartender.domain.models.base import BaseModel
-from bartender.domain.models.mixins.as_dict import DictSerializerMixin
-
-
-@dataclass(slots=True)
-class Tag(BaseModel, DictSerializerMixin):
-    name: str
